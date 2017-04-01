b237c839c2bec887471057f2d4f971e9af710de6
Ubtene <uz6@scarletmail.rutgers.edu>
2017-03-31 22:09:15 -0400

    meow

---
+++ Key.js
@@ -0,0 +1,15 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+
+  consumer_key: 'qLtYhCFy1oYwwRlQNnDD9HuyL',
+  consumer_secret: 'YEONWlx1owMqEih6jX93rTxl8dlvZj1ixyI5AYtW2AyxxtMRzm',
+  access_token_key: '59627538-VVa6Xm4GlGHlOpmQak7eNfa0C4ZpocpawVRmvlaQX',
+  access_token_secret: 'CbJk9uIa9wEymRchxiSO3NgHA4pJadiMzrbHdAqGrmaxE',
+
+
+});
+
+exports.key = client ;
+
+
---
+++ Liri.js
@@ -0,0 +1,138 @@
+var twitterKeys = require('./Key.js');
+
+var spotify = require('spotify');
+
+var fs = require("fs");
+
+var client = twitterKeys.key;
+
+var request = require('request');
+
+var thirdItem = process.argv[3];
+
+var secondItem = process.argv[2];
+
+if (secondItem === 'movie-this') {
+
+    if (!thirdItem) {
+
+        request('http://www.omdbapi.com/?t=Mr.%20Nobody%27', function(error, response, body) {
+
+
+            if (!error && response.statusCode == 200) {
+
+                // console.log(JSON.stringify(res, null, 2));
+
+                var obj = JSON.parse(body);
+
+                console.log(obj.Title); // Show the HTML for the Modulus homepage.
+                console.log(obj.Year);
+                console.log(obj.imdbRating);
+                console.log(obj.Country);
+                console.log(obj.Language);
+                console.log(obj.Plot);
+                console.log(obj.Actors);
+                console.log(obj.Ratings[1]);
+                console.log(obj.Website);
+            }
+
+
+        });
+
+    } else {
+
+
+        request('http://www.omdbapi.com/?t=' + thirdItem, function(error, response, body) {
+
+
+            if (!error && response.statusCode == 200) {
+
+                // console.log(JSON.stringify(res, null, 2));
+                var obj = JSON.parse(body);
+
+                console.log(obj.Title); // Show the HTML for the Modulus homepage.
+                console.log(obj.Year);
+                console.log(obj.imdbRating);
+                console.log(obj.Country);
+                console.log(obj.Language);
+                console.log(obj.Plot);
+                console.log(obj.Actors);
+                console.log(obj.Ratings[1]);
+                console.log(obj.Website);
+            }
+
+
+        });
+
+    }
+
+} 
+
+
+if (secondItem === "my-tweets") {
+
+    client.get('statuses/user_timeline', { include_entities: true, count: 20 },
+        function(err, tweets, response) {
+            if (err) {
+                console.log(err.toString());
+            } else {
+
+                for (var i = 0; i < tweets.length; i++) {
+                    console.log(JSON.stringify(tweets[i].text)),
+                        // console.log(JSON.stringify(data[i].text, null, 2)); 
+                        console.log('------------------------------------------')
+                }
+
+            }
+        });
+
+}
+
+
+if (secondItem === "spotify-this-song") {
+
+spotify.search({ type: 'track', query: thirdItem }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;  //from spotify npm docs
+    }
+    else{
+    var songInfo = data.tracks.items[0];
+    var songResult = console.log(songInfo.artists[0].name)
+                     console.log(songInfo.name)
+                     console.log(songInfo.album.name)
+                     console.log(songInfo.preview_url)
+    // console.log(songResult);
+    };
+  });
+
+      }
+
+
+if (secondItem === "hi") {
+
+fs.readFile('./random.txt', "utf8",  (err, data) => {
+  if (err) throw err;
+var read = data.split(",")
+
+spotify.search({ type: 'track', query: read[1] }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;  //from spotify npm docs
+    }
+    else{
+    var songInfo = data.tracks.items[0];
+    var songResult = console.log(songInfo.artists[0].name)
+                     console.log(songInfo.name)
+                     console.log(songInfo.album.name)
+                     console.log(songInfo.preview_url)
+    // console.log(songResult);
+    };
+  });
+
+
+
+
+});
+
+}
